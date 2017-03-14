17382d751f843a7fae4a707fb31ba5024f259238
James Volmert <james.volmert@gmail.com>
2017-03-13 19:01:35 -0600

    barely holding on

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
+/**
+   Create a component that creates HTML
+   Take component HTML and put it on browser
+ **/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function () {
+    return <h1>Hello</h1>;
+};  // const is variable declaration except it is a constant
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(App);
