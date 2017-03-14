1e075a151052c06dae9b48b25280a8ef1ed8adb3
James Volmert <james.volmert@gmail.com>
2017-03-13 19:39:51 -0600

    finally got dom element to render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 /**
    Create a component that creates HTML
    Take component HTML and put it on browser
+   Run npm start to start server
  **/
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function () {
+const App =  () => {
+    //  ^this arrow function replaces an anonymous function
+    //  it does change the definition of this however
+    
     return <h1>Hello</h1>;
 };  // const is variable declaration except it is a constant
 
-ReactDOM.render(App);
+ReactDOM.render(< App />, document.querySelector('.container') );
