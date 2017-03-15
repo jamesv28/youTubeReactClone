4605ed2b90e86c357fe6fc59d228bf89d1673775
jamesv28 <james.volmert@davita.com>
2017-03-15 13:49:00 -0600

    finshed a lesson

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    
-    return <input className="form-control"/>;
 
+    return <input className="form-control" onChange={this.onInputChange}/>;
+
+  }
+
+  onInputChange(e) {
+    console.log(e.target.value);
   }
 }
 
